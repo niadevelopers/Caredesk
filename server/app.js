06a55a6afe7e1513c97ffd2791841e9e07b55f6f
nia.team.developers <jobisaacmaina22@gmail.com>
2025-03-02 12:15:56 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+//app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Database connection
 db();

679297d960a2cfec9f0549549d51d00a7516f3e3
nia.team.developers <jobisaacmaina22@gmail.com>
2025-03-07 17:19:45 +0300

    Update login.js

--- client/js/login.js
+++ client/js/login.js
@@ -16,7 +16,7 @@ loginForm.addEventListener('submit', async (e) => {
     const result = await response.json();
     if (response.ok) {
       alert(result.message);
-      window.location.href = 'admin&id=67caf4bf-a60c-8008-9e8f-4b5bf502b3265450158,3523googledisplayjs?client=crossorigin.html';
+      window.location.href = 'admin67caf4bf-a60c-8008-9e8f-4b5bf502b32654501583523googledisplayjs?clientcrossorigin.html';
     } else {
       alert(result.error);
     }

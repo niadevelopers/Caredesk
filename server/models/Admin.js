23707cef2e72d866463eca28adec1e5988a3da8a
nia.team.developers <jobisaacmaina22@gmail.com>
2025-03-08 14:02:39 +0300

    Update Admin.js

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-const AdminSchema = new mongoose.Schema({
+const adminSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role: { type: String, enum: ['chief', 'junior'], required: true }, // Role-based system
 });
 
-module.exports = mongoose.model('Admin', AdminSchema);
+module.exports = mongoose.model('Admin', adminSchema);

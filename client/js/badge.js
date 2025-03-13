2d48d158886067c7f2c731a6b8fcbe91ad4d6401
nia.team.developers <jobisaacmaina22@gmail.com>
2025-03-13 16:27:22 +0300

    Update badge.js

--- client/js/badge.js
+++ client/js/badge.js
@@ -8,10 +8,13 @@ async function fetchBlogs() {
     const storedBlogCount = localStorage.getItem('blogCount') || 0;
     
     // If the fetched blogs are more than the stored count, it means there is a new blog
-    if (blogs.length > storedBlogCount) {
-      setNewBlogBadge(true, blogs.length);  // Pass the blog count to updateAppBadge
+    const isNewBlogAvailable = blogs.length > storedBlogCount;
+
+    // Force the display of the badge (you can show a toast as well)
+    if (isNewBlogAvailable) {
+      setNewBlogBadge(true, blogs.length);  // Show badge and set new blog count
     } else {
-      setNewBlogBadge(false, 0);  // No new blogs, set count to 0
+      setNewBlogBadge(false, 0);  // Hide badge if no new blogs
     }
     
     // Update the blog count in localStorage
@@ -25,16 +28,24 @@ async function fetchBlogs() {
 // Call the function when the page loads to check for new blogs
 document.addEventListener('DOMContentLoaded', fetchBlogs);
 
-// Function to show or hide the new blog badge
+// Function to show or hide the new blog badge (forceful)
 function setNewBlogBadge(isNewBlogAvailable, blogCount) {
   const badgeElement = document.getElementById('appBadge');  // The badge element in your UI (can be an icon or div)
-  
+  const toastElement = document.getElementById('toastNotification');  // A fallback toast notification element
+
   if (isNewBlogAvailable) {
-    // Show the badge or dot (you can style it as a dot or a number)
+    // Forcefully show the badge or dot (you can style it as a dot or a number)
     badgeElement.style.display = 'block';
+
+    // Show a toast notification to inform the user
+    toastElement.style.display = 'block';
+    toastElement.innerHTML = `${blogCount} New Blogs Available!`;  // Show the count in the toast
   } else {
     // Hide the badge if no new blog
     badgeElement.style.display = 'none';
+
+    // Hide the toast if no new blog
+    toastElement.style.display = 'none';
   }
   
   // Optionally update the browser tab (favicon or title)
@@ -45,7 +56,6 @@ function setNewBlogBadge(isNewBlogAvailable, blogCount) {
 function updateAppBadge(isNewBlogAvailable, blogCount) {
   if (isNewBlogAvailable) {
     document.title = `${blogCount} New Blogs`;  // Using the blog count instead of undefined 'count'
-    // You could also change the favicon to show a dot (using Favico.js or manually updating it)
   } else {
     document.title = "Caredesk";  // Reset title
   }
